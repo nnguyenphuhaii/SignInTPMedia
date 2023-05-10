cf704e3181a6102a995b9a9608240ac2feb3798c
Zeiko Nguyen <nnnnguyenphuhaiiii@gmail.com>
2023-05-10 07:35:06 +0700

    Finish Dang Nhap

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace SignInTPMedia.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
